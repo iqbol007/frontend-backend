e1f58abe4b1406a3a1997e63966e13aa8150b5aa
iqbol <iqbol@localhost>
2020-02-19 15:19:54 +0300

    something changed

--- app.js
+++ app.js
@@ -7,28 +7,17 @@ server.use(express.json());
 
 let nextPostId = 1;
 let posts = [
-  { id: nextPostId++, content: "ole", likes: 12 },
-  { id: nextPostId++, content: "kole", likes: 46 }
+  { id: nextPostId++, content: "ole", likes: 12,isLiked:false },
+  { id: nextPostId++, content: "kole", likes: 46,isLiked:false }
 ];
 function findPostIndexById(id) {
   return posts.findIndex(o => o.id === id);
 }
-// this is get requaire >>>>>>>>>
+ 
 server.get("/posts", (req, res) => {
   res.send(posts);
 });
-// server.get("/buggy", (req, res) => {
-//   setTimeout(() => {
-//     if (Math.random() * 0.5) {
-//       res.statusCode = 500;
-//       res.send();
-//       return;
-//     }
-//     res.send({ satatus: "ok" });
-//   }, Math.random() * 10000);
-// });
-
-//this is set or update request, if id===0 then create new post and else if id is define then post will be updated
+ 
 server.post("/posts", (req, res) => {
   const body = req.body;
   const id = body.id;
@@ -47,7 +36,7 @@ server.post("/posts", (req, res) => {
   posts = posts.map(o => (o.id !== id ? o : { ...o, content: body.content }));
   res.send(posts);
 });
-//delete requaire>>>>>>
+ 
 server.delete("/posts/:id", (req, res) => {
   const id = Number(req.params.id);
   const index = findPostIndexById(id);
@@ -58,27 +47,46 @@ server.delete("/posts/:id", (req, res) => {
   posts = posts.filter(o => o.id != id);
   res.send(posts);
 });
-///////////////like++
-server.post("/posts/:id/likes", (req, res) => {
-  const id = Number(req.params.id);
-  const index = findPostIndexById(id);
-  if (index === -1) {
-    res.status(404).send(erorforid);
-    return;
-  }
-  posts = posts.map(o => (o.id !== id ? o : { ...o, likes: o.likes + 1 }));
+ 
+// server.post("/posts/:id/likes", (req, res) => {
+//   const id = Number(req.params.id);
+//   const index = findPostIndexById(id);
+//   if (index === -1) {
+//     res.status(404).send(erorforid);
+//     return;
+//   }
+//   posts = posts.map(o => (o.id !== id ? o : { ...o, likes: o.likes + 1 }));
 
-  res.send(posts);
-});
-////////////////like--
-server.delete("/posts/:id/likes", (req, res) => {
-  const id = Number(req.params.id);
+//   res.send(posts);
+// });
+ 
+// server.delete("/posts/:id/likes", (req, res) => {
+//   const id = Number(req.params.id);
+//   const index = findPostIndexById(id);
+//   if (index === -1) {
+//     res.status(404).send(erorforid);
+//     return;
+//   }
+//   posts = posts.map(o => (o.id !== id ? o : { ...o, likes: o.likes - 1 }));
+//   res.send(posts);
+// });
+
+server.post("/posts/:id/likes", (req, res) => {
+    const id = Number(req.params.id);
   const index = findPostIndexById(id);
   if (index === -1) {
     res.status(404).send(erorforid);
     return;
   }
-  posts = posts.map(o => (o.id !== id ? o : { ...o, likes: o.likes - 1 }));
-  res.send(posts);
-});
+  posts = posts.map(o => 
+     {if ( o.id !== id) {
+      return o;
+     }
+     o.isLiked=!o.isLiked;
+     return { ...o, likes:o.isLiked? o.likes - 1:o.likes+1 };
+    
+    });
+      res.send(posts);
+  });
+
 server.listen(process.env.PORT || 9999);
